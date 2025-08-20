f20c8de88e4f3854807d2e36f0ee1fcdbc6c40e5
Mellachervu Dharani <mellachervudharani07@gmail.com>
2025-08-20 14:20:44 +0530

    The kafka producer and the consumer

--- ingestion-service/src/main/java/com/dharani/ingestion/IngestionServiceApplication.java
+++ ingestion-service/src/main/java/com/dharani/ingestion/IngestionServiceApplication.java
@@ -2,12 +2,23 @@ package com.dharani.ingestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @SpringBootApplication
 public class IngestionServiceApplication {
 
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(IngestionServiceApplication.class, args);
-	}
+
+
+
+
+    }
 
 }
